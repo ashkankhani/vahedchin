e2074c04362155668e7e961fa85d6731db2d6ab1
Ashkan Khani <drashkankhani@gmail.com>
2023-01-30 19:20:06 +0330

    write plan section

--- main.py
+++ main.py
@@ -15,6 +15,10 @@ class Lesson:
         self.day = re.findall(r'^\w+',detail)[0]
         self.start = datetime.strptime(times[0],'%H:%M')
         self.end = datetime.strptime(times[1],'%H:%M')
+    def conflits(self,other):
+        if(other.start<=self.start<other.end or self.start<=other.start<self.end):
+            return True
+        return False
     def __str__(self):
         return f'{self.day}|{self.start}|{self.end}'
 
@@ -23,6 +27,22 @@ for index,row in cdf.iterrows():
     myDict.setdefault(row['نام درس'],[])
     myDict[row['نام درس']].append(Lesson(row['زمانبندي تشکيل کلاس']))
 
+validPlans = []
 
-for key,value in myDict.items():
-    print(value[0])
+for choices in product(*myDict.values()):
+    temp = []
+    ok = True
+    for les in choices:
+        if(ok):
+            for les2 in temp:
+                if(les.conflits(les2)):
+                    ok = False
+                    break
+            if(ok):
+                temp.append(les)
+    if(ok):
+        temp.sort(key=lambda x:x.start)
+        validPlans.append(temp)                
+
+
+print(validPlans)
